abe968630d1c037daa4806889f5d5062b7d7d97d
marianne-butaye <marianne.butaye@gmail.com>
2016-12-01 23:05:30 +0100

    commit marianne

---
+++ Kilobits_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kilobits_iOS
+//
+//  Created by marianne Butaye on 01/12/2016.
+//  Copyright © 2016 Club Info. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
