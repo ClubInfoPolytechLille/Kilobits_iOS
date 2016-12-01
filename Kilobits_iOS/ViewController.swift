43642bc5444e4efd2cda38137832a62f66fa73d5
florian-deze <florian.deze@gmail.com>
2016-12-01 23:18:56 +0100

    deze is on fire

--- Kilobits_iOS/ViewController.swift
+++ Kilobits_iOS/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("deze is on fire")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
